64af36af9498e9e78eb746601dd462f9f1be7abe
haya14busa <haya14busa@gmail.com>
2020-01-19 05:17:32 +0000

    initial version

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,22 +3,22 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('test runs (true)', () => {
+  process.env['INPUT_COND'] = 'true'
+  process.env['INPUT_IF_TRUE'] = 'value-if-true'
+  process.env['INPUT_IF_FALSE'] = 'value-if-false'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
+
+test('test runs (false)', () => {
+  process.env['INPUT_COND'] = 'false'
+  process.env['INPUT_IF_TRUE'] = 'value-if-true'
+  process.env['INPUT_IF_FALSE'] = 'value-if-false'
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
